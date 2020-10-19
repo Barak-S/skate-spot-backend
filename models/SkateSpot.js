ed696b2721044bb9898fd4f32a3dda144b4dec24
Barak-S <barak.saidoff@gmail.com>
2020-10-19 18:00:21 -0400

    modify post request route

--- models/SkateSpot.js
+++ models/SkateSpot.js
@@ -4,7 +4,6 @@ const Schema = mongoose.Schema;
 const SkateSpotSchema = new Schema({
   name: {
     type: String,
-    required: true
   },
   address: {
     type: String,
--- routes/api/skatespots.js
+++ routes/api/skatespots.js
@@ -4,26 +4,21 @@ const router = express.Router();
 
 const SkateSpot = require("../../models/SkateSpot");
 
-
 router.get('/skatespots', (req, res)=>{
-
-    SkateSpot.find({}, function(err, result) {
-        if (err) {
-          console.log(err);
-        } else {
-          res.json(result);
-        }
-      });
+  SkateSpot.find({}, function(err, result) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(result);
+    }
+  });
 })
 
 router.post('/skatespots/create', (req,res)=>{
-    // console.log(req.body.newSpot)
     let newSpot = new SkateSpot(req.body.newSpot)    
     newSpot.save()
-    // .then(() => console.log("saved NEW PARK succesfully", newSpot))
     .then(newPark => res.send(newPark))
     .catch(error => console.log(error));
-
 })
 
 router.get('/skatespots/:id',(req,res)=>{
